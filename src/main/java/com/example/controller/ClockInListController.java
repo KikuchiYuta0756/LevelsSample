eec4f96f56bc51166055c40c343eaf8fc31868eb
KikuchiYuta0756 <kikuyu0756@gmail.com>
2024-01-06 20:25:42 +0900

    2024/1/6 COMMIT

--- src/main/java/com/example/controller/ClockInListController.java
+++ src/main/java/com/example/controller/ClockInListController.java
@@ -1,11 +1,15 @@
 package com.example.controller;
+
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.example.domainUser.model.WorkTimeEntity;
 import com.example.domainUser.service.WorkTimeService;
@@ -32,4 +36,12 @@ public class ClockInListController {
 	return "user/clockInList";
 	}
 
+		//勤怠修正画面の表示
+		@GetMapping("/clockCorrection")
+		public String getClockCorrection(){
+			
+						
+		return "user/clockCorrection";
+		}
+	
 }
