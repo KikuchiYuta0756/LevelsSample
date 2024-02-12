e8220d5e917d0571ec5ff11d4032418520c393a0
KikuchiYuta0756 <kikuyu0756@gmail.com>
2024-02-13 08:23:59 +0900

    2024/2/13 COMMIT

--- src/main/java/com/example/controller/ClockInController.java
+++ src/main/java/com/example/controller/ClockInController.java
@@ -5,37 +5,57 @@ import java.time.format.DateTimeFormatter;
 //import java.time.Month;
 //import java.text.SimpleDateFormat;
 
-
+import org.modelmapper.ModelMapper;
 //import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 //import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domainUser.model.UserMapperEntity;
 import com.example.domainUser.model.WorkTimeEntity;
+import com.example.domainUser.service.UserService;
 import com.example.domainUser.service.WorkTimeService;
 //import com.example.form.WorkTimeForm;
+import com.example.form.UserDetailForm;
 
 
 //import lombok.extern.slf4j.Slf4j;
-
-
 
 @Controller
 @RequestMapping("/user")
 //@Slf4j
 public class ClockInController {
+		
+	@Autowired
+	private UserService userService;
 	
 	@Autowired
 	private WorkTimeService workTimeService;
-		
-	//@Autowired
-	//private ModelMapper modelMapper; 
+	
+	@Autowired
+	private ModelMapper modelMapper;
+
 	
 	//勤怠打刻画面（ユーザ用表）示
 	@GetMapping("/clockIn")
-	public String getClockIn(){
+	public String getClockIn(UserDetailForm form,
+			Model model){
+		
+		//認証ユーザーの出退勤フラグを取得
+		UserMapperEntity userWorkFlg = userService.getWorkFlg();
+		
+		System.out.println(userWorkFlg);
+
+		//UserMapperEntityをformに変換
+		form = modelMapper.map(userWorkFlg, UserDetailForm.class);
+
+
+		// Modelに登録
+		model.addAttribute("userWorkFlg", form);
 				
 		//勤怠打刻画面に遷移
 	return "user/clockIn";
@@ -50,22 +70,12 @@ public class ClockInController {
 		//LocalDateTimeで現在日時の取得
 		LocalDateTime ldtnow = LocalDateTime.now();
 		
-		//15分単位に丸める plusMinutes
-		//LocalDateTime adjustdedDateTime = ldtnow.truncatedTo(ChronoUnit.MINUTES)
-		//		.plusMinutes(ldtnow.getMinute() % 15 == 0 ? 0 : 15 - ldtnow.getMinute() % 15);
-		
-		// 分を10分単位に丸めてから5捨6入する
-//        LocalDateTime adjustedDateTime = ldtnow.truncatedTo(ChronoUnit.MINUTES)
-//                                             .plusMinutes(Math.round(ldtnow.getMinute() / 10.0) * 10);
-
         // 分を10分単位に変換
         int minute = ldtnow.getMinute();
         int roundedMinute = Math.round((float) minute / 10) * 10;
 
         // 新しい LocalDateTime を作成して、分を変更
         LocalDateTime adjustedDateTime = ldtnow.withMinute(roundedMinute);
-
-		
 		
 		//Date出力形式を指定
 		DateTimeFormatter dtfdate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
@@ -81,7 +91,10 @@ public class ClockInController {
 
 		workTimeService.startTimeSignup(worktime);
 		
-		return "redirect:/user/clockIn";
+		//出退勤フラグ（退勤）を更新する
+		userService.getWorkFlgLeaving();
+		
+		return "redirect:/user/clockIn";	
 	}
 	
 	//退勤ボタン処理
@@ -90,14 +103,6 @@ public class ClockInController {
 		
 		LocalDateTime ldtnow = LocalDateTime.now();
 		
-		//15分単位に丸める minusMinutes
-//		LocalDateTime adjustdedDateTime = ldtnow.truncatedTo(ChronoUnit.MINUTES)
-//				.minusMinutes(ldtnow.getMinute() % 15);
-		
-		// 分を10分単位に丸めてから5捨6入する
-//        LocalDateTime adjustedDateTime = ldtnow.truncatedTo(ChronoUnit.MINUTES)
-//                                             .plusMinutes(Math.round(ldtnow.getMinute() / 10.0) * 10);
-
         // 分を10分単位に変換
         int minute = ldtnow.getMinute();
         int roundedMinute = Math.round((float) minute / 10) * 10;
@@ -125,12 +130,12 @@ public class ClockInController {
 		WorkTimeEntity worktimeentity = new WorkTimeEntity();
 		worktimeentity.setWorkDate(strdate);		
 		
-		//労働時間のDB登録
-		//workTimeService.genWorkTimeSignup(worktimeentity);
-		//実働時間のDB登録
-		//workTimeService.actWorkTimeSignup(worktimeentity);
 		//残業時間のDB登録
 		workTimeService.overTimeSignup(worktimeentity);
+		
+		//出退勤フラグ（出勤）を更新する
+		userService.getWorkFlgAttendance();
+
 		
 		
 		
