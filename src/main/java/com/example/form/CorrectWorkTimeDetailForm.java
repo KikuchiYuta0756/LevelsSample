a617afcd4cc1581968c1e9d16259d31a4c68586a
KikuchiYuta0756 <kikuyu0756@gmail.com>
2024-02-28 03:46:18 +0900

    2024/2/28 勤怠修正画面

---
+++ src/main/java/com/example/form/CorrectWorkTimeDetailForm.java
@@ -0,0 +1,31 @@
+package com.example.form;
+
+import java.time.LocalTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class CorrectWorkTimeDetailForm {
+	private String workDate;
+	private Integer dayofweekId;
+	private String loginId;
+	@DateTimeFormat(pattern = "hh:mm")
+	private String startTime;
+	@DateTimeFormat(pattern = "hh:mm")
+	private String closeTime;
+	
+	@DateTimeFormat(pattern = "hh:mm")
+    private LocalTime genWorkTime;
+
+	@DateTimeFormat(pattern = "hh:mm")
+	private LocalTime restTime;
+	
+	@DateTimeFormat(pattern = "hh:mm")
+	private LocalTime actWorkTime;
+
+	@DateTimeFormat(pattern = "hh:mm")
+	private LocalTime overTime;
+
+}
